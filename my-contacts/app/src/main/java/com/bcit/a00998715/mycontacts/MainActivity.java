d0ad39290f4e2c0dd6045ba48185a0b4e0b29b42
Edgar Zapeka <edz@edgars-mbp.wifi.bcit.ca>
2018-03-02 10:59:15 -0800

    Initialized new project

---
+++ my-contacts/app/src/main/java/com/bcit/a00998715/mycontacts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bcit.a00998715.mycontacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
